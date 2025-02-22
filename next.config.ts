7a0dea91ed85da2e354c9a23f1a861ee48f7d27b
b47labs <admin@b47labs.com>
2025-02-22 09:46:28 -0500

    update image basepaths

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
+  basePath: "/b47labs.com",
 };
 
 export default nextConfig;
